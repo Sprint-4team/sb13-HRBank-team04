e94ab732f355c171bab0a1f5244db673a25a92b0
marcel615 <alswo000615@gmail.com>
2026-07-13 09:21:55 +0900

    feat: 백업 스케줄러
    - BackupScheduler를 만들어서 이후 백업이 한 시간마다 자동으로 이루어지도록 구현
    -

---
+++ src/main/java/com/codeit/hrbank/backup/scheduler/BackupScheduler.java
@@ -0,0 +1,24 @@
+package com.codeit.hrbank.backup.scheduler;
+
+import com.codeit.hrbank.backup.service.BackupHistoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BackupScheduler {
+
+    private final BackupHistoryService backupHistoryService;
+
+    @Scheduled(fixedDelayString = "3600000")
+    public void autoBackup() {
+        log.info("자동 백업 시작");
+
+        backupHistoryService.createBackupHistory("system");
+
+        log.info("자동 백업 종료");
+    }
+}
