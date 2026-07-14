e0621415c13f80d369a93ccef3a06ec90e2dc09a
hisjeans <his7jeans@gmail.com>
2026-07-14 17:37:15 +0900

    refactor: 파일 서비스 예외 처리 리팩토링 및 FileNotFoundException 메시지 수정

--- src/main/java/com/codeit/hrbank/file/exception/FileNotFoundException.java
+++ src/main/java/com/codeit/hrbank/file/exception/FileNotFoundException.java
@@ -8,7 +8,7 @@ public class FileNotFoundException extends BusinessException {
   public FileNotFoundException(Long id) {
     super(HttpStatus.NOT_FOUND, "파일을 찾을 수 없습니다.: "+ id);
   }
-  public FileNotFoundException(String path){
-    super(HttpStatus.NOT_FOUND, "파일을 찾을 수 없습니다.: "+ path);
+  public FileNotFoundException() {
+    super(HttpStatus.NOT_FOUND, "파일이 비어 있습니다.");
   }
 }
