18795039cd9b2db58491b330d8fde8d589f6795b
BaekHanCheon <qor3296@gmail.com>
2026-07-15 09:41:45 +0900

    feat : SwaggerConfig 생성, 타이틀과 github 링크 생성

---
+++ src/main/java/com/codeit/hrbank/global/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.codeit.hrbank.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI discodeitOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("4팀 포그라운드 HR Bank API.")
+            .description("4팀의 HR Bank API 문서.")
+            .version("v1.0.0")
+            .contact(new Contact()
+                .name("github - sb13-HRBank-team04")
+                .url("https://github.com/Sprint-4team/sb13-HRBank-team04")
+            ));
+  }
+}
