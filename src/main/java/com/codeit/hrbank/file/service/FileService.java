b7beea4d505ed274b953d16761b28abdcb8a17cf
hisjeans <his7jeans@gmail.com>
2026-07-15 18:16:41 +0900

    refactor: FileService → LocalFileStorage로 저장 책임을 분리 및 FileDownLoadDto 적용

--- src/main/java/com/codeit/hrbank/file/service/FileService.java
+++ src/main/java/com/codeit/hrbank/file/service/FileService.java
@@ -1,12 +1,12 @@
 package com.codeit.hrbank.file.service;
 
+import com.codeit.hrbank.file.dto.FileDownloadDto;
 import com.codeit.hrbank.file.entity.File;
-import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
   File createFile(MultipartFile file);
   File findFile(Long id);
-  Resource downloadFile(Long id);
+  FileDownloadDto downloadFile(Long id);
   void deleteFile(Long id);
 }
